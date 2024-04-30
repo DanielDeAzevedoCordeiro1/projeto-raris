60c2f1ccab4c9c495b1b29c4fd04f9ed87473464
Lizandra de Jesus Ferrari <lizandra.ferrari@hotmail.com>
2024-04-30 18:34:27 -0300

    navbar estilizada e remoção 1/2 de páginas css extra

--- components/header.js
+++ components/header.js
@@ -5,26 +5,42 @@ class Header extends HTMLElement {
   
     connectedCallback() {
       this.innerHTML = `
-      <nav class="navbar navbar-light bg-light justify-content-between">
-      <a class="navbar-brand">Airis logo</a>
-        <a>Calculadora</a>
-        <ul>
-        <li class="nav-item dropdown">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="layout.css">
+      </head>
+      <body>
+      <nav class="navbar navzinha justify-content-between">
+      <ul>
+      <a href = "index.html">LOGOAIRIS</a>
+      </ul>
+      <ul>
+        <a>Informações</a>
+        </ul>
+        <ul class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Informações
-        </a>
+          Calculadora</a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Beneficios de ser MEI</a>
+          <a class="dropdown-item" href="#">Impostos</a>
+          <div class="dropdown-divider"></div> <!-- é só uma barrinha -->
+          <a class="dropdown-item" href="#">Receita</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Taxas</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Taxa de</a>
         </div>
-      </li></ul>
+      </ul>
 
         <ul>
-        <a>Login</a>
-        <a>Cadastro</a>
+        <a href = "testecadastro.html">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+  <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"/>
+</svg>
+        Cadastro</a>
+
+        <a href = "testelogin.html">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+      </svg>
+      Login</a>
 </ul>
     </nav>
 `
